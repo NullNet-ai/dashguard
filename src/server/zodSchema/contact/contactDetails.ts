8e713180a9ec3876861cb90c07ec6746f87a0f39
Irish <irufo@dnamicro.com>
2024-12-12 11:56:58 +0800

    added main grid in contact

--- src/server/zodSchema/contact/contactDetails.ts
+++ src/server/zodSchema/contact/contactDetails.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
-export const basicDetailsSchema = z.object({
+export const contactDetailsSchema = z.object({
+  id: z.string().min(1),
   first_name: z.string().min(1),
   last_name: z.string().min(1),
   middle_name: z.string().nullable(),
-  id: z.string().min(1),
 });
