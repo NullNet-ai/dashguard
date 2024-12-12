3c760bb5d5b195f1f82fe963073d9ea5f1d7e9fc
Jane <jrepollo@dnamicro.com>
2024-12-12 12:20:49 +0800

    Update contact phone and email validation schemas

--- src/server/api/routers/contact.ts
+++ src/server/api/routers/contact.ts
@@ -1,10 +1,17 @@
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 import { z } from "zod"; // Zod is used for input validation
 import ZodItems from "~/server/zodSchema/grid/items";
-import { EOperator, IAdvanceFilters } from "@dna-platform/common-orm";
+import {
+  EOperator,
+  EOrderDirection,
+  IAdvanceFilters,
+} from "@dna-platform/common-orm";
 import { formatSorting } from "~/server/utils/formatSorting";
 import { pick } from "lodash";
 import { ContactCategoryDetailsSchema } from "~/server/zodSchema/contact/categoryDetails";
+import { ContactPhoneEmailSchema } from "../../zodSchema/contact/contactPhoneEmail";
+import { EStatus } from "../types";
+import { createAdvancedFilter } from "../../utils/transformAdvanceFilter";
 
 const ENTITY = "contact";
 
@@ -162,5 +169,223 @@ export const contactRouter = createTRPCRouter({
           },
         })
         .execute();
+    }),
+  updateContactPhoneEmail: privateProcedure
+    .input(ContactPhoneEmailSchema)
+    .mutation(async ({ input, ctx }) => {
+      const { id, email, phone } = input;
+
+      let contact_id = id;
+      let contact_code = "";
+      if (!contact_id) {
+        const record = await ctx.dnaClient
+          .create({
+            entity: "contact",
+            token: ctx.token.value,
+            mutation: {
+              params: {
+                status: "Draft",
+              },
+              pluck: ["id", "code"],
+            },
+          })
+          .execute();
+        contact_id = record?.data?.[0]?.id;
+        contact_code = record?.data?.[0]?.code;
+      }
+
+      const insert = async (entity: string, data: any, pluck: string[]) => {
+        const record = await ctx.dnaClient
+          .create({
+            entity,
+            token: ctx.token.value,
+            mutation: {
+              params: { ...data, contact_id, status: "Active" },
+              pluck,
+            },
+          })
+          .execute();
+        return record?.data?.[0];
+      };
+
+      const response = await Promise.all([
+        insert("contact_email", email?.[0], [
+          "email",
+          "id",
+          "contact_id",
+          "is_primary",
+        ]),
+        insert("contact_phone_number", phone?.[0], [
+          "raw_phone_number",
+          "id",
+          "contact_id",
+          "is_primary",
+          "iso_code",
+          "country_code",
+        ]),
+      ]);
+      console.info("[Insert Contact Phone Email]", response);
+
+      return {
+        data: {
+          id: contact_id,
+          code: contact_code,
+          email: [response[0]],
+          phone: [response[1]],
+        },
+      };
+    }),
+  fetchContactPhoneEmail: privateProcedure
+    .input(
+      z.object({
+        id: z.string().min(1),
+        pluck_fields: z.array(z.string()),
+        main_entity: z.string().min(1),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const { total_count: totalCount = 1, data: items } = await ctx.dnaClient
+        .findAll({
+          entity: input.main_entity,
+          token: ctx.token.value,
+          query: {
+            pluck: input.pluck_fields,
+            pluck_object: {
+              contact_emails: ["email", "is_primary"],
+              contact_phone_numbers: [
+                "raw_phone_number",
+                "iso_code",
+                "country_code",
+                "is_primary",
+              ],
+              contacts: input.pluck_fields,
+            },
+            advance_filters: [
+              {
+                field: "code",
+                operator: EOperator.EQUAL,
+                values: [input.id],
+              },
+            ] as IAdvanceFilters[],
+            order: {
+              starts_at: 0,
+              limit: 1,
+              by_field: "created_date",
+              by_direction: EOrderDirection.DESC,
+            },
+          },
+        })
+        .join({
+          type: "left",
+          field_relation: {
+            to: {
+              entity: "contact_emails",
+              field: "contact_id",
+            },
+            from: {
+              entity: "contacts",
+              field: "id",
+            },
+          },
+        })
+        .join({
+          type: "left",
+          field_relation: {
+            to: {
+              entity: "contact_phone_numbers",
+              field: "contact_id",
+            },
+            from: {
+              entity: "contacts",
+              field: "id",
+            },
+          },
+        })
+        .execute();
+
+      return {
+        data: {
+          id: items?.[0]?.contacts?.id,
+          email: items?.[0]?.contact_emails ? [items?.[0]?.contact_emails] : [],
+          phone: items?.[0]?.contact_phone_numbers
+            ? [items?.[0]?.contact_phone_numbers]
+            : [],
+        },
+      };
+    }),
+  getBasicDetails: privateProcedure
+    .input(
+      z.object({
+        code: z.string(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      if (!input?.code) return null;
+
+      const record = await ctx.dnaClient
+        .findAll({
+          entity: "contact",
+          token: ctx.token.value,
+          query: {
+            advance_filters: [
+              {
+                type: "criteria",
+                field: "code",
+                operator: EOperator.EQUAL,
+                values: [input.code],
+              },
+            ],
+            pluck: ["id", "first_name", "middle_name", "last_name", "goes_by"],
+          },
+        })
+        .execute();
+
+      const advance_filters = createAdvancedFilter({
+        contact_id: record?.data?.[0]?.id,
+        status: EStatus.ACTIVE,
+        is_primary: true,
+      });
+      const order = {
+        starts_at: 0,
+        limit: 1,
+        by_field: "created_date",
+        by_direction: EOrderDirection.DESC,
+      };
+      const [contact_emails, contact_phone_numbers] = await Promise.all([
+        ctx.dnaClient
+          .findAll({
+            entity: "contact_email",
+            token: ctx.token.value,
+            query: {
+              advance_filters,
+              order,
+              pluck: ["email"],
+            },
+          })
+          .execute(),
+        ctx.dnaClient
+          .findAll({
+            entity: "contact_phone_number",
+            token: ctx.token.value,
+            query: {
+              advance_filters,
+              order,
+              pluck: ["raw_phone_number"],
+            },
+          })
+          .execute(),
+      ]);
+
+      const { raw_phone_number = "" } = contact_phone_numbers?.data?.[0] || {};
+
+      const primary_phone_number: string = "+" + raw_phone_number;
+
+      return {
+        data: {
+          ...record?.data?.[0],
+          ...contact_emails?.data?.[0],
+          primary_phone_number,
+        },
+      };
     }),
 });
