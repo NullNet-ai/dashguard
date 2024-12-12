614cb4d0f6fc105697fa9769eb132baf49e0e461
Jane <jrepollo@dnamicro.com>
2024-12-12 12:24:49 +0800

    feat: Add ContactDetails form component for server-side rendering

---
+++ src/app/portal/contact/_components/form-filter/contact-details/actions.ts
@@ -0,0 +1,44 @@
+"use server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { api } from "~/trpc/server";
+
+export const saveContactDetails = async (data: any, action_type?: string) => {
+  const response = await api.contact.updateContactPhoneEmail(data);
+
+  if(action_type === 'Create') {
+    const headerList = headers();
+    const pathname = headerList.get("x-pathname") || "";
+    const [, portal, mainEntity] = pathname.split("/");
+    const currentContext = "/" + portal + "/" + mainEntity;
+    await api.tab.closeCurrentInnerClassTab({
+      href: pathname,
+      current_context: currentContext,
+    });
+  }
+  return [response.data];
+};
+
+export const selectRecord = async (rows: any[]) => {
+  const headerList = headers();
+  const pathname = headerList.get("x-pathname") || "";
+  const [, portal, mainEntity] = pathname.split("/");
+  const currentContext = "/" + portal + "/" + mainEntity;
+  await api.tab.closeCurrentInnerClassTab({
+    href: pathname,
+    current_context: currentContext,
+  });
+  redirect(`/portal/${mainEntity}/wizard/${rows?.[0]?.code}/1`);
+};
+
+export const removeRecord = async () => {
+  const headerList = headers();
+  const pathname = headerList.get("x-pathname") || "";
+  const [, portal, mainEntity] = pathname.split("/");
+  const currentContext = "/" + portal + "/" + mainEntity;
+  await api.tab.closeCurrentInnerClassTab({
+    href: pathname,
+    current_context: currentContext,
+  });
+  redirect(`/portal/${mainEntity}/wizard/new/1`);
+};
