b1380c0890471f6dfab49f67a124a68aad14e79e
Irish <irufo@dnamicro.com>
2024-12-12 13:08:16 +0800

    added contact details summary

---
+++ src/app/portal/contact/wizard/(summary)/_2/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+import useRefetchRecord from "../hooks/useFetchMainRecord";
+import { api } from "~/trpc/react";
+
+const fields = {
+  "First Name": "first_name",
+  "Last Name": "last_name",
+  "Middle Name": "middle_name",
+};
+
+const ContactDetailsSummary = ({
+  form_key,
+  identifier,
+  main_entity,
+}: {
+  form_key: string;
+  identifier: string;
+  main_entity: string;
+}) => {
+  const {
+    data: record = { data: { id: null } },
+    refetch,
+    error,
+  } = api.record.getByCode.useQuery({
+    main_entity: main_entity!,
+    id: identifier!,
+    pluck_fields: ["id", "first_name", "last_name", "middle_name"],
+  });
+
+  const { data } = record || {};
+
+  useRefetchRecord({
+    refetch,
+    form_key,
+  });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      {Object.entries(fields).map(([key, value]) => (
+        <p key={key}>
+          <strong> {key}: </strong>
+          &nbsp; {data[value] || "None"}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default ContactDetailsSummary;
--- src/server/zodSchema/contact/contactDetails.ts
+++ src/server/zodSchema/contact/contactDetails.ts
@@ -2,7 +2,19 @@ import { z } from "zod";
 
 export const contactDetailsSchema = z.object({
   id: z.string().min(1),
-  first_name: z.string().min(1),
-  last_name: z.string().min(1),
+  first_name: z
+    .string({
+      message: "First Name is required.",
+    })
+    .min(1, {
+      message: "First Name is required.",
+    }),
+  last_name: z
+    .string({
+      message: "Last Name is required.",
+    })
+    .min(1, {
+      message: "Last Name is required.",
+    }),
   middle_name: z.string().nullable(),
 });
