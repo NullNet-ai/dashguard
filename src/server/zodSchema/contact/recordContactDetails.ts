9fa099c10b3c602026297d79383ed6ac36e6d9f2
Irish <irufo@dnamicro.com>
2024-12-13 14:14:41 +0800

    extend types zod

--- src/server/zodSchema/contact/recordContactDetails.ts
+++ src/server/zodSchema/contact/recordContactDetails.ts
@@ -1,23 +1,7 @@
-import { z } from "zod";
 import { EmailArraySchema, PhoneArraySchema } from "./contactPhoneEmail";
+import { contactDetailsSchema } from "./contactDetails";
 
-export const recordContactDetailsSchema = z.object({
-  id: z.string().min(1),
-  first_name: z
-    .string({
-      message: "First Name is required.",
-    })
-    .min(1, {
-      message: "First Name is required.",
-    }),
-  last_name: z
-    .string({
-      message: "Last Name is required.",
-    })
-    .min(1, {
-      message: "Last Name is required.",
-    }),
-  middle_name: z.string().nullable(),
-  phone: PhoneArraySchema,
-  email: EmailArraySchema,
+export const recordContactDetailsSchema = contactDetailsSchema.extend({
+  emails: EmailArraySchema,
+  phones: PhoneArraySchema,
 });
