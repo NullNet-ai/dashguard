c901c23760918b73d04e1166f66b4e4489afc9eb
Juphet Vitualla <jvitualla@dnamicro.com>
2024-12-13 13:51:56 +0800

    hide search

--- src/components/platform/EnhancedFormBuilder/components/custom/FormFilter/List.tsx
+++ src/components/platform/EnhancedFormBuilder/components/custom/FormFilter/List.tsx
@@ -54,7 +54,6 @@ export default function FormFilterGrid({
   }
 
   const initialSelectedRecords = selectedRecords.reduce((acc, id) => ({...acc, [id]: true}), {});
-
   return (
     <Grid
       onSelectRecords={(rows) => {
