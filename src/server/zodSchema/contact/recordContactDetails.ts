4c0e80ef78ebfe7178b3d755d09a0ee9b7d9ac16
Irish <irufo@dnamicro.com>
2024-12-13 09:00:33 +0800

    added record details for contact tab

---
+++ src/server/zodSchema/contact/recordContactDetails.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { EmailArraySchema, PhoneArraySchema } from "./contactPhoneEmail";
+
+export const recordContactDetailsSchema = z.object({
+  id: z.string().min(1),
+  first_name: z
+    .string({
+      message: "First Name is required.",
+    })
+    .min(1, {
+      message: "First Name is required.",
+    }),
+  last_name: z
+    .string({
+      message: "Last Name is required.",
+    })
+    .min(1, {
+      message: "Last Name is required.",
+    }),
+  middle_name: z.string().nullable(),
+  phone: PhoneArraySchema,
+  email: EmailArraySchema,
+});
