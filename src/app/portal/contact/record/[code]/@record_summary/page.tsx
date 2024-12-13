83f68952dce20b273c761395d345afe9f7c2e4d9
Irish <irufo@dnamicro.com>
2024-12-13 10:01:16 +0800

    added phone formatter

--- src/app/portal/contact/record/[code]/@record_summary/page.tsx
+++ src/app/portal/contact/record/[code]/@record_summary/page.tsx
@@ -1,7 +1,49 @@
 import RecordSummary from "~/components/platform/RecordV2/Summary/RecordSummary";
+import RecordShellSummary from "../../_components/record-shell-summary";
+import { headers } from "next/headers";
+import { api } from "~/trpc/server";
+import { formatPhoneNumber } from "~/utils/formatter";
 
-export default function Page() {
-    return (
-        <RecordSummary />
-    );
+export default async function Page() {
+  const headerList = headers();
+  const pathname = headerList.get("x-pathname") || "";
+  const [, , , , identifier] = pathname.split("/");
+
+  const record_details = await api.contact.fetchContactPhoneEmail({
+    code: identifier!,
+    pluck_fields: ["id", "first_name", "last_name", "categories"],
+  });
+
+  const { categories, first_name, last_name, email, phone } = record_details;
+
+  const full_name = `${first_name} ${last_name}`;
+
+  const _categories = categories
+    .filter((cat: string) => cat !== "Contact")
+    .join(", ");
+
+  const _email = email?.find(
+    ({ is_primary }: { is_primary: boolean }) => is_primary,
+  );
+
+  const _phone = phone?.find(
+    ({ is_primary }: { is_primary: boolean }) => is_primary,
+  );
+
+  const { raw_phone_number, iso_code } = _phone || {};
+  const format_phone = formatPhoneNumber({
+    raw_phone_number,
+    iso_code,
+  });
+  return (
+    <div>
+      <RecordSummary />
+      <RecordShellSummary
+        email={_email?.email}
+        phone={format_phone}
+        full_name={full_name}
+        categories={_categories}
+      />
+    </div>
+  );
 }
