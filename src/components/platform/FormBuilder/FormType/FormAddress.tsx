e5a7bac78d3e9836ea262274003cd21e1ccb91d4
Jane <jrepollo@dnamicro.com>
2024-12-17 07:52:49 +0800

    Refactor FormAddress component and update props

--- src/components/platform/FormBuilder/FormType/FormAddress.tsx
+++ src/components/platform/FormBuilder/FormType/FormAddress.tsx
@@ -1,17 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  ControllerFieldState,
-  ControllerRenderProps,
-  type UseFormReturn,
-} from "react-hook-form";
+import { type UseFormReturn } from "react-hook-form";
 import AddressAutoComplete from "~/components/platform/AddressAutoComplete";
-import { IField } from "../type";
 
 interface IProps {
   form: UseFormReturn<Record<string, any>, any, undefined>;
-  fieldConfig? : any
-  formKey : string
-  formRenderProps?: any
+  fieldConfig?: any;
+  formKey: string;
+  formRenderProps?: any;
 }
 
 export default function FormAddress({
