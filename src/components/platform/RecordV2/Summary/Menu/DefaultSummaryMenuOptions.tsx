942b011160a9c5aa3e26ea6525f4578bba88541b
Franklin Okiya <franklinokiya@dnamicro.com>
2024-12-17 08:40:42 +0800

    Add status type definitions and implement status change handling in contact and template pages

--- src/components/platform/RecordV2/Summary/Menu/DefaultSummaryMenuOptions.tsx
+++ src/components/platform/RecordV2/Summary/Menu/DefaultSummaryMenuOptions.tsx
@@ -9,18 +9,45 @@ import {
 } from "~/components/ui/dropdown-menu";
 import RecursiveMenuItem from "./RecursiveMenuItem";
 import { DEFAULT_MENU_OPTION_CONFIG } from "../../constants";
+import { ReactNode, useMemo } from "react";
+
+
+interface IMemoizedRecordData {
+  entityName?: string;
+  identifier?: string;
+  status: string;
+  recordId: string;
+}
+
+interface IMenuOptionConfig {
+  label: string;
+  onClick: (recordId: string, entityName: string) => void;
+}
+
+interface IDefaultSummaryMenuOptionsProps {
+  title?: string;
+  memoizedRecordData: IMemoizedRecordData;
+  menuOptionConfig?: IMenuOptionConfig[];
+}
+
 
 export default function DefaultSummaryMenuOptions({
   title,
-  recordId,
-  entityName,
-}: {
-  recordId: string;
-  entityName: string;
-  title?: string;
-}) {
+  memoizedRecordData,
+  menuOptionConfig,
+}: IDefaultSummaryMenuOptionsProps) {
+
+  const { recordId, entityName } = memoizedRecordData;
+
+  const memoizedMenuOptionConfig = useMemo(() => {
+    return [
+      ...DEFAULT_MENU_OPTION_CONFIG,
+      ...(menuOptionConfig || [])
+    ]
+  }, [menuOptionConfig]);
+  
   return (
-    <DropdownMenu>
+    <DropdownMenu> 
       <DropdownMenuTrigger asChild>
         <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
           <EllipsisVertical className={`h-4 w-4`} aria-hidden="true" />
@@ -28,7 +55,11 @@ export default function DefaultSummaryMenuOptions({
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         {title && <DropdownMenuLabel>{title}</DropdownMenuLabel>}
-        <RecursiveMenuItem recordId={recordId} entityName={entityName} menuOptionConfig={DEFAULT_MENU_OPTION_CONFIG} />
+        <RecursiveMenuItem 
+          recordId={recordId} 
+          entityName={entityName!} 
+          menuOptionConfig={memoizedMenuOptionConfig} 
+        />
       </DropdownMenuContent>
     </DropdownMenu>
   );
