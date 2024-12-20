1a69fba8c9acc12c5913c27d812588a20aea2931
jupiter <jvitualla@dnamicro.com>
2024-12-20 09:51:17 +0800

    Enhance FormFilterGrid responsiveness and integrate sidebar context

--- src/components/platform/EnhancedFormBuilder/components/custom/FormFilter/List.tsx
+++ src/components/platform/EnhancedFormBuilder/components/custom/FormFilter/List.tsx
@@ -5,6 +5,10 @@ import Grid from "../../../../Grid/Client";
 
 import Skeleton from "../../../../Grid/Skeleton";
 import { IFilterGridConfig } from "../../../types/global/interfaces";
+import { useSidebar } from "~/components/ui/sidebar";
+import { cn } from "~/lib/utils";
+import { WizardContext } from "~/components/platform/Wizard/Provider";
+import { useContext, useMemo } from "react";
 
 export default function FormFilterGrid({
   config,
@@ -53,6 +57,13 @@ export default function FormFilterGrid({
     grid_total_count = totalCount || 0;
   }
   handleListLoading(isLoading);
+  const {state} = useContext(WizardContext)
+  const {open} = useSidebar()
+
+  
+
+
+  
   if (isLoading) {
     return (
       <div
@@ -70,8 +81,28 @@ export default function FormFilterGrid({
     (acc, id) => ({ ...acc, [id]: true }),
     {},
   );
+
+
+  const calcWidth = useMemo(() => {
+    if (open && state?.isSummaryOpen) {
+      return 'w-[calc(100vw-530px)]'
+    }
+    else if (!open && state?.isSummaryOpen) {
+      return 'w-auto'
+    }
+    else if (open && !state?.isSummaryOpen) {
+      return 'w-[calc(100vw-320px)]'
+    }
+    else
+      return ''
+  }, [open, state?.isSummaryOpen])
+
+
   return (
-    <Grid
+   <div className={
+    cn(`${calcWidth}`)
+   }>
+     <Grid
       height="300px"
       onSelectRecords={(rows) => {
         if (!onSelectRecords) return;
@@ -119,5 +150,6 @@ export default function FormFilterGrid({
       }}
       initialSelectedRecords={initialSelectedRecords}
     />
+   </div>
   );
 }
