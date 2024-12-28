d226dc728925e8a7b64f9f6047f5317c6c9f2080
juphet <juphetvitualla@dnamicro.com>
2024-12-28 16:24:45 +0800

    Refactor header components for improved layout responsiveness and consistency

--- src/components/platform/Tab/Item.tsx
+++ src/components/platform/Tab/Item.tsx
@@ -21,9 +21,9 @@ const Item = ({ tab }: ItemProps) => {
           aria-current={tab.current ? "page" : undefined}
           className={cn(
             tab.current
-              ? "rounded-t-lg border-b-0 border-l border-r border-t-2 border-t-primary text-primary"
+              ? "md:rounded-t-lg md:border-b-0 md:border-l md:border-r md:border-t-2 md:border-t-primary text-primary"
               : "text-gray-500",
-            "whitespace-nowrap px-4 pt-2  text-sm font-medium",
+            "whitespace-nowrap px-4 md:pt-2 py-1.5 text-sm font-medium",
             "flex items-center space-x-2",
             "hover:border-t-primary hover:text-primary",
             padding
