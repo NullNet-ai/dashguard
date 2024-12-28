c95d2d79a689815b463672de42bb43df6cef9e94
juphet <juphetvitualla@dnamicro.com>
2024-12-28 09:01:30 +0800

    fixed error removed params

--- src/app/portal/dashboard/layout.tsx
+++ src/app/portal/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout(props: {children: React.ReactNode}) {
 
-    const { params, children, ...rest } = props
+    const { children, ...rest } = props
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
             {...Object.values(rest)}
