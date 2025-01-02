8c07e3023f2d8f3256c9e48c556553160c0e1da3
Jane <jrepollo@dnamicro.com>
2025-01-03 07:43:21 +0800

    Convert Step 1 to Form Filter

---
+++ src/app/portal/(settings)/user_role/_components/form-filter/basic-details/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { api } from "~/trpc/server";
+
+export const selectRecord = async (rows: any[]) => {
+  const headerList = headers();
+  const pathname = headerList.get("x-pathname") || "";
+  const [, portal, mainEntity] = pathname.split("/");
+  const currentContext = "/" + portal + "/" + mainEntity;
+  await api.tab.closeCurrentInnerClassTab({
+    href: pathname,
+    current_context: currentContext,
+  });
+  redirect(`/portal/${mainEntity}/wizard/${rows?.[0]?.code}/1`);
+};
+
+export const removeRecord = async () => {
+  const headerList = headers();
+  const pathname = headerList.get("x-pathname") || "";
+  const [, portal, mainEntity] = pathname.split("/");
+  const currentContext = "/" + portal + "/" + mainEntity;
+  await api.tab.closeCurrentInnerClassTab({
+    href: pathname,
+    current_context: currentContext,
+  });
+  redirect(`/portal/${mainEntity}/wizard/new/1`);
+};
