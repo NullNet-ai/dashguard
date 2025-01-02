2f2e90989728da514f573029fa5a3d6b130751ea
Kit <kitmikhaelbagares@dnamicro.com>
2025-01-03 03:25:32 +0800

    Update: added accuracy types

--- src/components/platform/FormBuilder/types/global/interfaces.ts
+++ src/components/platform/FormBuilder/types/global/interfaces.ts
@@ -98,6 +98,7 @@ interface IField {
     field?: string;
   };
   selectSearchable?: boolean;
+  accuracy?: number;
 }
 
 interface ISelectOptions {
@@ -181,7 +182,11 @@ interface IFilterGridConfig {
   label?: string;
   gridColumns: ColumnDef<any>[];
   actionType: TActionType;
-  onClipboardPaste?: (data: Record<string, any>, form: any, onSubmitFormGrid?: any) => any;
+  onClipboardPaste?: (
+    data: Record<string, any>,
+    form: any,
+    onSubmitFormGrid?: any,
+  ) => any;
   renderComponentSelected?: (record: any) => JSX.Element;
   onSelectRecords?: ({
     rows,
@@ -303,7 +308,6 @@ interface IFieldFilterActions {
   ref?: any;
 }
 
-
 export type {
   IButtonConfig,
   ICheckboxOptions,
@@ -317,5 +321,5 @@ export type {
   ISelectOptions,
   IUserFormField,
   OptionType,
-  IFieldFilterActions
+  IFieldFilterActions,
 };
