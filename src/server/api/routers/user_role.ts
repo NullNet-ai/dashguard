f6586e7e9196bec6536e2fd81da8d1f19d7f9a62
Jane <jrepollo@dnamicro.com>
2025-01-03 08:31:06 +0800

    feat: Add saveUserRole mutation to user_roles router

--- src/server/api/routers/user_role.ts
+++ src/server/api/routers/user_role.ts
@@ -1,11 +1,107 @@
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 import { createDefineRoutes } from "../baseCrud";
 import { z } from "zod";
-import { EOrderDirection } from "@dna-platform/common-orm";
+import { EOperator, EOrderDirection } from "@dna-platform/common-orm";
 import { createAdvancedFilter } from "~/server/utils/transformAdvanceFilter";
+import { UserRoleFormSchema } from "../../zodSchema/user_role/basicDetails";
 
 export const userRolesRouter = createTRPCRouter({
   ...createDefineRoutes("user_roles"),
+  saveUserRole: privateProcedure
+    .input(UserRoleFormSchema.extend({ id: z.string().optional() }))
+    .mutation(async ({ input, ctx }) => {
+      const { role, id: user_role_id } = input;
+
+      const roles = await ctx.dnaClient
+        .findAll({
+          entity: "user_roles",
+          token: ctx.token.value,
+          query: {
+            pluck: ["id", "status"],
+            advance_filters: [
+              ...createAdvancedFilter({ role }),
+              ...(user_role_id
+                ? [
+                    {
+                      operator: EOperator.AND,
+                      type: "operator",
+                    },
+                    {
+                      field: "id",
+                      operator: EOperator.NOT_EQUAL,
+                      type: "criteria",
+                      values: [user_role_id],
+                    },
+                  ]
+                : []),
+            ],
+            order: {
+              limit: 1,
+              by_field: "created_date",
+              by_direction: EOrderDirection.DESC,
+            },
+          },
+        })
+        .execute();
+
+      if (roles?.data?.length) {
+        const [role] = roles.data;
+        const { id: existing_id, status } = role || {};
+        return {
+          message: "Role already exists.",
+          data: [],
+          status_code: 409,
+          total_count: 0,
+          record_count: 0,
+          existing: true,
+          existing_record: {
+            id: existing_id,
+            status,
+          },
+          errors: {
+            form: [
+              {
+                field: "role",
+                message: "Role already exists.",
+              },
+            ],
+          },
+        };
+      }
+
+      if (!user_role_id) {
+        const record = await ctx.dnaClient
+          .create({
+            entity: "user_role",
+            token: ctx.token.value,
+            mutation: {
+              params: {
+                status: "Draft",
+                role,
+              },
+              pluck: ["id", "code"],
+            },
+          })
+          .execute();
+
+        return record;
+      }
+
+      const res = await ctx.dnaClient
+        .update(user_role_id!, {
+          entity: "user_role",
+          token: ctx.token.value,
+          mutation: {
+            params: {
+              role,
+            },
+            pluck: ["id", "code", "role"],
+          },
+        })
+        .execute();
+
+      return res;
+    }),
   update: privateProcedure
     .input(
       z.object({
