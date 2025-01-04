71474493aa9d5725d8b1c04ca62c05760d79437b
Franklin Okiya <franklinokiya@dnamicro.com>
2025-01-04 11:59:22 +0800

    refactor: remove archive option from CHANGE_RECORD_STATE menu

--- src/components/platform/Record/constants/index.ts
+++ src/components/platform/Record/constants/index.ts
@@ -1,4 +1,3 @@
-import archive from "../actions/archive";
 import { IMenuOptionConfig } from "../types";
 
 export const CHANGE_RECORD_STATE = "Change Record State";
@@ -8,14 +7,6 @@ export const DEFAULT_MENU_OPTION_CONFIG: IMenuOptionConfig[] = [
   {
     label: CHANGE_RECORD_STATE,
     onClick: () => ({}),
-    children: [
-      {
-        label: "Archive",
-        onClick: async (recordId: string, entityName: string) => {
-          await archive(recordId, entityName);
-        },
-        children: [],
-      },
-    ],
+    children: [],
   },
 ];
