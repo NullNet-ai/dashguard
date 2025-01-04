5ed514581cdcd351e31c540c447e3380796b61fd
Rob Wilson <robcaldosa@dnamicro.com>
2025-01-04 12:45:46 +0800

    Refactor MinimalTiptapEditor component styles to add focus ring

--- src/components/ui/rich-text-editor/minimal-tiptap.tsx
+++ src/components/ui/rich-text-editor/minimal-tiptap.tsx
@@ -161,7 +161,7 @@ export const MinimalTiptapEditor = React.forwardRef<
         name="editor"
         ref={ref}
         className={cn(
-          "flex h-auto min-h-72 w-full flex-col rounded-md border border-input shadow-sm focus-within:border-primary",
+          "flex h-auto min-h-72 w-full flex-col rounded-md border border-input shadow-sm focus-within:border-primary focus-within:ring-primary focus-within:ring-1",
           className,
         )}
       >
