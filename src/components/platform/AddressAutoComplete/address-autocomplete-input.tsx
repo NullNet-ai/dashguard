a57f357f851a593fd2ac54e18287e3b1ed6ba26c
Malyn Gamboa <malyngamboa@dnamicro.com>
2025-01-10 11:08:34 +0800

    update remove ts-expect error

--- src/components/platform/AddressAutoComplete/address-autocomplete-input.tsx
+++ src/components/platform/AddressAutoComplete/address-autocomplete-input.tsx
@@ -48,7 +48,7 @@ interface CommonProps {
   }) => void;
 }
 
-export function  AddressAutoCompleteInput(props: CommonProps) {
+export function AddressAutoCompleteInput(props: CommonProps) {
   const { handleSelectAddress, form, formKey, fieldConfig, formRenderProps } =
     props;
   const googleAutoComplete = api.google.searchPlace.useMutation();
@@ -84,7 +84,6 @@ export function  AddressAutoCompleteInput(props: CommonProps) {
       name="inp-addr"
       render={(formRenderProps) => {
         const is_readonly =
-          //@ts-expect-error - should add readonly to IField
           (formRenderProps.field.readonly || fieldConfig?.readonly) ?? false;
         const is_disabled =
           formRenderProps.field.disabled || fieldConfig?.disabled;
@@ -112,7 +111,7 @@ export function  AddressAutoCompleteInput(props: CommonProps) {
                     readOnly={is_readonly}
                     data-test-id={formKey + "-" + formRenderProps?.field.name}
                     autoComplete="off"
-                    className="relative h-10 w-full focus:border-primary focus:ring-primary flex-grow rounded-md border border-border bg-transparent pl-11 pr-4 text-foreground placeholder:text-muted-foreground focus:border sm:text-sm"
+                    className="relative h-10 w-full flex-grow rounded-md border border-border bg-transparent pl-11 pr-4 text-foreground placeholder:text-muted-foreground focus:border focus:border-primary focus:ring-primary sm:text-sm"
                     placeholder="Search..."
                     onFocus={open}
                     onChange={(event) => {
