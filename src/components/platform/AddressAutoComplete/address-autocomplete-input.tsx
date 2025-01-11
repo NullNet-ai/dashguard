11b62fd9fef656f3b2afc7167864518f56092a10
Rob Wilson <robcaldosa@dnamicro.com>
2025-01-11 12:16:33 +0800

    Improve code

--- src/components/platform/AddressAutoComplete/address-autocomplete-input.tsx
+++ src/components/platform/AddressAutoComplete/address-autocomplete-input.tsx
@@ -84,7 +84,7 @@ export function AddressAutoCompleteInput(props: CommonProps) {
       name="inp-addr"
       render={(formRenderProps) => {
         const is_readonly =
-          (formRenderProps.field.disabled || fieldConfig?.readonly) ?? false;
+        !!(formRenderProps.field.disabled || fieldConfig?.readonly)
         const is_disabled = fieldConfig?.disabled;
         const is_disabled_or_readonly = is_disabled || is_readonly;
         return (
