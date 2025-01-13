9cb3ddd5a31b5a1a4e9caa37823901a5068f2603
Juphet vitualla <vituallajuphet1114@gmail.com>
2025-01-13 17:53:13 +0800

    Refactor: Improve Grid Header and Sorting components for better layout and responsiveness

--- src/components/platform/Grid/Header/index.tsx
+++ src/components/platform/Grid/Header/index.tsx
@@ -11,13 +11,13 @@ import Sorting from "../Sorting";
 export default function Header() {
   return (
     <>
-      <div className="flex flex-col-reverse gap-y-4 py-2 pb-0 lg:flex-row">
-        <div className="flex flex-col justify-between sm:flex-auto lg:flex-row">
-          <div>
+      <div className="flex flex-col-reverse gap-y-4 py-2 pb-0 lg:flex-row lg:justify-between">
+        <div className="flex flex-col justify-between sm:flex-auto lg:flex-row md:max-w-[53%]">
+          <div className="flex-1 w-full flex flex-col"> 
             <div className="flex h-[40px] justify-between">
               <GridTabs />
             </div>
-          <Sorting />
+           <Sorting />
           </div>
         </div>
         <Search />
