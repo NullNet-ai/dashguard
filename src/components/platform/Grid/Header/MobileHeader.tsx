15946f3ce25e72f76d564cb7efcb00dcf1cc1320
Juphet vitualla <vituallajuphet1114@gmail.com>
2025-01-22 08:25:46 +0800

    update: simplify MobileHeader component structure

--- src/components/platform/Grid/Header/MobileHeader.tsx
+++ src/components/platform/Grid/Header/MobileHeader.tsx
@@ -4,11 +4,9 @@ import Sorting from "../Sorting";
 
 export default function MobileHeader() {
   return (
-    <>
-      <div className="flex flex-col py-2 pb-0 lg:flex-row lg:justify-between">
-        <Search />
-        <Sorting />
-      </div>
-    </>
+    <div className="flex flex-col py-2 pb-0 lg:flex-row lg:justify-between">
+      <Search />
+      <Sorting />
+  </div>
   );
 }
