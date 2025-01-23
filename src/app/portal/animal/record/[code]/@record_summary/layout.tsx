6078af578a81bdc57e0e6d0f34da3323d3a152e2
Kit <kitmikhaelbagares@dnamicro.com>
2025-01-24 07:09:04 +0800

    update:tab retemplate

--- src/app/portal/animal/record/[code]/@record_summary/layout.tsx
+++ src/app/portal/animal/record/[code]/@record_summary/layout.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-
 import type { ILayoutProps } from "./types";
-
-const Layout = (props: ILayoutProps) => {
-  const { children } = props;
-  return <div>{children}</div>;
-};
-
+const Layout = ({ children }: ILayoutProps) => <div>{children}</div>;
 export default Layout;
--- src/app/portal/animal/record/[code]/@record_summary/page.tsx
+++ src/app/portal/animal/record/[code]/@record_summary/page.tsx
@@ -1,5 +1,4 @@
 import RecordSummary from "~/components/platform/Record/Summary/RecordSummary";
-
 export default function Page() {
   return <RecordSummary />;
 }
