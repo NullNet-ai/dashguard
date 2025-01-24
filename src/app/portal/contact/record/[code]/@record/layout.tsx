53a340088aba693d4b91b4db05a4379475664e3d
Kit <kitmikhaelbagares@dnamicro.com>
2025-01-25 05:58:52 +0800

    Update: contact migrated

--- src/app/portal/contact/record/[code]/@record/layout.tsx
+++ src/app/portal/contact/record/[code]/@record/layout.tsx
@@ -3,15 +3,11 @@ import React from "react";
 import { useSearchParams } from "next/navigation";
 
 import { ILayoutProps } from "./types";
-
+import ComingSoon from "../../_components/coming_soon";
 const Layout = (props: ILayoutProps) => {
   const searchParams = useSearchParams();
-
   const slot = props[searchParams.get("current_tab") ?? "dashboard"];
-
-  if (!slot) {
-    return <div>Coming Soon</div>;
-  }
+  if (!slot) return <ComingSoon />;
   return <div>{slot}</div>;
 };
 
