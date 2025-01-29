a32a9460805e37ac7166f2904702ab08f3162f29
rob caldosa <robcaldosa@dnamicro.com>
2025-01-29 10:12:11 +0800

    fix and update to use isActive instead of tab.current in checking the active state

--- src/components/platform/Tab/Item.tsx
+++ src/components/platform/Tab/Item.tsx
@@ -1,62 +1,57 @@
-"use client";
+'use client'
 
-import { Fragment, useMemo } from "react";
-import { cn } from "~/lib/utils";
-import CloseTab from "./CloseKebab";
-import { formatAndCapitalize } from "~/lib/utils";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-type ItemProps = {
-  tab: any;
-};
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { Fragment, useMemo } from 'react'
 
-const Item = ({ tab }: ItemProps) => {
-  const padding = tab.name === "dashboard" ? "pr-2" : "pr-0";
-  const checkIfUserRole = (entity: string) =>
-    entity === "user_role" ? true : false;
+import { cn, formatAndCapitalize } from '~/lib/utils'
 
-  const pathname = usePathname();
-  const [, , entity] = pathname?.split("/");
+import CloseTab from './CloseKebab'
+interface ItemProps {
+  tab: any
+}
+
+const Item = (props: ItemProps) => {
+  const padding = props.tab.name === 'dashboard' ? 'pr-4' : 'pr-0'
+  const checkIfUserRole = (entity: string) => entity === 'user_role' ? true : false
+
+  const pathname = usePathname()
+  const [, , entity] = pathname ? pathname.split('/') : ['', '', '']
 
   const isActive = useMemo(() => {
-    const [, , entityName] = (tab.href || "").split("/");
-    return entityName === entity;
-  }, [entity]);
+    const [, , entityName] = (props.tab.href || '').split('/')
+    return entityName === entity
+  }, [entity])
 
   return (
-    <Fragment key={checkIfUserRole(tab.name) ? "role" : tab.name}>
+    <Fragment key={checkIfUserRole(props.tab.name) ? 'role' : props.tab.name}>
       <div className="group relative flex items-center">
         <Link
-          data-test-id={
-            "mntab-" +
-            (checkIfUserRole(tab.name) ? "role" : tab.name)
-              .split(" ")
-              .join("-")
-              .toLowerCase()
-          }
-          href={tab.href}
-          aria-current={isActive ? "page" : undefined}
+          aria-current={isActive ? 'page' : undefined}
           className={cn(
-            tab.current
-              ? "text-primary rounded-t-lg border-b-0 border-l border-r border-t-2 border-t-primary"
-              : "text-gray-500",
-            "whitespace-nowrap px-[8px] py-1 text-sm font-medium  max-h-[32px]",
-            "flex items-center pl-[8px] space-x-2",
-            "hover:border-t-primary hover:text-primary relative",
-            padding,
+            isActive
+              ? 'text-primary md:rounded-t-lg md:border-b-0 md:border-l md:border-r md:border-t-2 md:border-t-primary'
+              : 'text-gray-500', 'whitespace-nowrap px-4 py-1.5 text-sm font-medium md:pt-2', 'flex items-center space-x-2', 'hover:border-t-primary hover:text-primary', padding,
           )}
+          data-test-id={
+            `mntab-${
+              (checkIfUserRole(props.tab.name) ? 'role' : props.tab.name)
+                .split(' ')
+                .join('-')
+                .toLowerCase()}`
+          }
+          href={props.tab.href}
         >
-          {formatAndCapitalize(checkIfUserRole(tab.name) ? "role" : tab.name)}
-          {}
-          <CloseTab {...tab} />
+          {formatAndCapitalize(checkIfUserRole(props.tab.name) ? 'role' : props.tab.name)}
+          <CloseTab {...props.tab} />
         </Link>
 
-        {isActive || tab.current && (
-          <div className="absolute bottom-[-4px] z-10 h-1 w-full bg-white"></div>
+        {isActive && (
+          <div className="absolute bottom-[-10px] z-10 h-1 w-full bg-white" />
         )}
       </div>
     </Fragment>
-  );
-};
+  )
+}
 
-export default Item;
+export default Item
