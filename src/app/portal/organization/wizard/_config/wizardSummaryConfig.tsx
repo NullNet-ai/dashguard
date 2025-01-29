93770a8339b5e53ad2056db41ea2aad325ecaff2
Mary Grace Tiburillo <mgtiburillo@dnamicro.com>
2025-01-29 08:49:22 +0800

    Update: migrate new template to contact and organization

--- src/app/portal/organization/wizard/_config/wizardSummaryConfig.tsx
+++ src/app/portal/organization/wizard/_config/wizardSummaryConfig.tsx
@@ -1,16 +1,11 @@
 import one from "../(summary)/_1";
 import two from "../(summary)/_2";
-import three from "../(summary)/_2";
-import four from "../(summary)/_2";
-import five from "../(summary)/_2";
+
 
 const WizardSummaryComponent = () => {
   return {
     one,
     two,
-    three,
-    four,
-    five,
   };
 };
 
