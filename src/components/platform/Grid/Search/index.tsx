07b13eba0bc19dbc502fbc0d176062abb69c4538
Juphet vitualla <vituallajuphet1114@gmail.com>
2025-01-29 09:19:13 +0800

    update eslint error

--- src/components/platform/Grid/Search/index.tsx
+++ src/components/platform/Grid/Search/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import ButtonHeader from '../Header/ButtonHeader'
+import CreateButton from '../Header/ButtonHeader'
 import CardViewButton from '../Header/CardViewButton'
 import FilterButton from '../Header/FilterButton'
 import TableViewButton from '../Header/TableViewButton'
@@ -10,12 +10,13 @@ import SortingList from './SortingList'
 import SortingListMobile from './SortingListMobile'
 import Search from './View'
 
-export default function Main(props: any) {
+// eslint-disable-next-line react/destructuring-assignment
+export default function Main({ parentType = 'grid' }: any) {
   return (
     <GridSearchProvider>
-      {props.parentType === 'grid'
+      {parentType === 'grid'
         ? (
-            <div className="ml-0 mt-0 flex w-full max-w-[100%] flex-col justify-end gap-x-2 sm:mt-0 lg:ml-2 lg:mt-0 lg:w-[40%] lg:max-w-[40%]">
+            <div className="ml-0 mt-0 flex w-full max-w-[100%] flex-col justify-end gap-x-2 sm:mt-0 lg:ml-2 lg:mt-4 lg:w-[40%] lg:max-w-[40%]">
               <div className="relative flex flex-1 flex-row gap-x-2">
                 <div className="my-2 h-[40px] w-full md:my-0">
                   <Search />
@@ -26,10 +27,7 @@ export default function Main(props: any) {
                   <div className="mx-2 h-full w-[1px] bg-tertiary" />
                   <FilterButton />
                 </div>
-                <ButtonHeader
-                  className="hidden lg:inline-flex"
-                  title="New"
-                />
+                <CreateButton className="hidden lg:inline-flex" title="New" />
               </div>
               <div className="hidden min-h-[40px] lg:block">
                 <SortingList />
