67b16201f4651251858df5202bd41f0b71a9b97e
Irish <irufo@dnamicro.com>
2025-01-30 11:14:27 +0800

    added esLint

--- src/app/portal/device/_components/chart/GridBarGraph.tsx
+++ src/app/portal/device/_components/chart/GridBarGraph.tsx
@@ -1,35 +1,36 @@
-"use client"
+'use client'
 
-import { Bar, BarChart } from "recharts"
+import React from 'react'
+import { Bar, BarChart } from 'recharts'
 
-import { ChartConfig, ChartContainer } from "~/components/ui/chart"
+import { type ChartConfig, ChartContainer } from '~/components/ui/chart'
 
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
 ]
 
 const chartConfig = {
   desktop: {
-    label: "Desktop",
-    color: "#008000",
+    label: 'Desktop',
+    color: '#008000',
   },
   mobile: {
-    label: "Mobile",
-    color: "#008000",
+    label: 'Mobile',
+    color: '#008000',
   },
 } satisfies ChartConfig
 
 export function GridBarGraph() {
   return (
-    <ChartContainer config={chartConfig} className="min-h-[10px] w-30">
-      <BarChart accessibilityLayer data={chartData}>
-        <Bar dataKey="desktop" fill="var(--color-desktop)" />
-        <Bar dataKey="mobile" fill="var(--color-mobile)" />
+    <ChartContainer className='min-h-[10px] w-30' config={chartConfig}>
+      <BarChart accessibilityLayer={true} data={chartData}>
+        <Bar dataKey='desktop' fill='var(--color-desktop)' />
+        <Bar dataKey='mobile' fill='var(--color-mobile)' />
       </BarChart>
     </ChartContainer>
   )
---
+++ src/app/portal/device/_components/record_configuration/ConfigurationAliasGrid/_config/columns.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import { type ColumnDef } from '@tanstack/react-table'
+import React from 'react'
+
+import StatusCell from '~/components/ui/status-cell'
+
+const gridColumns = [
+  {
+    header: 'State',
+    accessorKey: 'status',
+    enableResizing: false,
+    cell: ({ row }) => {
+      const value = row?.original?.status
+      return <StatusCell value={value} />
+    },
+  },
+  {
+    header: 'Status',
+    accessorKey: 'configuration_rule_status',
+    enableResizing: false,
+    cell: ({ row }) => {
+      const value = row?.original?.device_status
+      return <StatusCell value={value} />
+    },
+  },
+  {
+    header: 'Type',
+    accessorKey: 'configuration_type',
+  },
+  {
+    header: 'Action',
+    accessorKey: '',
+  },
+  {
+    header: 'Protocol',
+    accessorKey: '',
+  },
+  {
+    header: 'Source',
+    accessorKey: '',
+  },
+  {
+    header: 'Src Port',
+    accessorKey: 'ip_address',
+  },
+
+  {
+    header: 'Destination',
+    accessorKey: '',
+  },
+  {
+    header: 'Dest Port',
+    accessorKey: '',
+  },
+  {
+    header: 'Description',
+    accessorKey: '',
+  },
+  {
+    header: 'Updated Date',
+    accessorKey: 'updated_date',
+    cell: ({ row }) => {
+      const date = row?.original?.updated_date
+      const time = row?.original?.updated_time
+      return (
+        <div className='flex items-center gap-x-2'>
+          <div>{date}</div>
+          <div>{time}</div>
+        </div>
+      )
+    },
+  },
+  {
+    header: 'Updated By',
+    accessorKey: 'updated_by',
+    sortKey: 'updated_by.first_name',
+    search_config: {
+      entity: 'updated_by',
+      field: 'first_name',
+      operator: 'like',
+    },
+  },
+  {
+    header: 'Created Date',
+    accessorKey: 'created_date',
+    cell: ({ row }) => {
+      const date = row?.original?.created_date
+      const time = row?.original?.created_time
+      return (
+        <div className='flex items-center gap-x-2'>
+          <div>{date}</div>
+          <div>{time}</div>
+        </div>
+      )
+    },
+  },
+  {
+    header: 'Created By',
+    accessorKey: 'created_by',
+    sortKey: 'created_by.first_name',
+    search_config: {
+      entity: 'created_by',
+      field: 'first_name',
+      operator: 'like',
+    },
+  },
+] as ColumnDef<any>[]
+
+export default gridColumns
+
+// ? You can add columns to hide when mobile view as per your requirement just copy the respective accessorKey from the gridColumns
+export const TO_HIDE_COLUMNS_WHEN_MOBILE = []
