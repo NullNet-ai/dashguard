4eeb47186cb9038f4861dff0809e553a36da970e
Irish <irufo@dnamicro.com>
2025-01-31 05:34:32 +0800

    modify model field, should not be editable once saved

--- src/app/portal/device/_components/forms/_custom/CustomBasicDetails/index.tsx
+++ src/app/portal/device/_components/forms/_custom/CustomBasicDetails/index.tsx
@@ -1,237 +1,240 @@
-import { Fragment, useEffect, useMemo, useState } from "react";
-import { type UseFormReturn } from "react-hook-form";
-import { type IDropdown } from "~/app/portal/contact/_components/forms/category-details/types";
-import FormSelect from "~/components/platform/FormBuilder/FormType/FormSelect";
-import { FormField, FormItem } from "~/components/ui/form";
-import countriesToCities from "~/components/platform/AddressAutoComplete/countriesToCities.json";
+import React, { useEffect, useMemo, useState } from 'react'
+import { type UseFormReturn } from 'react-hook-form'
+
+import { type IDropdown } from '~/app/portal/contact/_components/forms/category-details/types'
+import countriesToCities from '~/components/platform/AddressAutoComplete/countriesToCities.json'
 import {
   countryList,
   getStates,
-} from "~/components/platform/AddressAutoComplete/countryStates";
-import FormInput from "~/components/platform/FormBuilder/FormType/FormInput";
+} from '~/components/platform/AddressAutoComplete/countryStates'
+import FormInput from '~/components/platform/FormBuilder/FormType/FormInput'
+import FormSelect from '~/components/platform/FormBuilder/FormType/FormSelect'
+import { FormField, FormItem } from '~/components/ui/form'
 
 interface BasicDetails {
   defaultValues?: Partial<{
-    model: string;
-    grouping: string;
-    country: string;
-    city: string;
-    state: string;
-    id: string;
-  }>;
-  form: UseFormReturn<Record<string, any>, any, undefined>;
+    model: string
+    grouping: string
+    country: string
+    city: string
+    state: string
+    id: string
+  }>
+  form: UseFormReturn<Record<string, any>, any, undefined>
   options?: {
-    appendFormKey?: string;
-  };
+    appendFormKey?: string
+  }
   selectOptions: {
-    model: IDropdown[];
-    grouping: IDropdown[];
-  };
+    model: IDropdown[]
+    grouping: IDropdown[]
+  }
+  disabledModel: boolean
 }
-const transformDropdown = (data: string[]) =>
-  data?.map((item) => ({ value: item, label: item }));
+const transformDropdown = (data: string[]) => data?.map(item => ({ value: item, label: item }))
 
 export default function CustomBasicDetails({
   form,
   selectOptions,
+  disabledModel,
 }: BasicDetails) {
-  const { control, watch } = form;
-  const { model, grouping } = selectOptions;
+  const { control, watch } = form
+  const { model, grouping } = selectOptions
 
-  const [city_options, setCityOptions] = useState<IDropdown[]>([]);
+  const [city_options, setCityOptions] = useState<IDropdown[]>([])
 
-  const [state_options, setStateOptions] = useState<IDropdown[]>([]);
+  const [state_options, setStateOptions] = useState<IDropdown[]>([])
 
   const country_options = useMemo(() => {
     return countryList.map((country: string) => ({
       value: country,
       label: country,
-    }));
-  }, []);
+    }))
+  }, [])
 
-  const selected_country = watch("country") as "China";
+  const selected_country = watch('country') as 'China'
 
   useEffect(() => {
-    if (!selected_country) return;
-    const cities = countriesToCities?.[selected_country] as string[];
-    const city_options = transformDropdown(cities);
-    setCityOptions(city_options);
+    if (!selected_country) return
+    const cities = countriesToCities?.[selected_country] as string[]
+    const city_options = transformDropdown(cities)
+    setCityOptions(city_options)
 
-    const states = getStates(selected_country) as string[];
-    const state_options = transformDropdown(states);
-    setStateOptions(state_options);
-  }, [selected_country]);
+    const states = getStates(selected_country) as string[]
+    const state_options = transformDropdown(states)
+    setStateOptions(state_options)
+  }, [selected_country])
 
   return (
     <FormField
-      name="Basic Details"
       control={form.control}
+      name="Basic Details"
       render={() => {
         return (
           <FormItem>
-            <Fragment>
+            <>
               <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
                 <FormField
-                  name={`model`}
                   control={control}
+                  name='model'
                   render={(formProps) => {
                     return (
                       <FormSelect
-                        formKey="device_model"
                         fieldConfig={{
                           id: `model`,
-                          formType: "select",
+                          formType: 'select',
                           name: `model`,
-                          label: "Model",
+                          label: 'Model',
                           required: true,
+                          disabled: disabledModel,
                         }}
-                        formRenderProps={formProps}
                         form={form}
+                        formKey="device_model"
+                        formRenderProps={formProps}
                         selectOptions={{
-                          model: model,
+                          model,
                         }}
                       />
-                    );
+                    )
                   }}
                 />
                 <FormField
-                  name={`instance_name`}
                   control={control}
+                  name='instance_name'
                   render={(formProps) => {
                     return (
                       <FormInput
-                        formKey="device_instance_name"
                         fieldConfig={{
                           id: `instance_name`,
-                          formType: "input",
+                          formType: 'input',
                           name: `instance_name`,
-                          label: "Instance Name",
+                          label: 'Instance Name',
                           required: true,
                         }}
-                        formRenderProps={formProps}
                         form={form}
+                        formKey="device_instance_name"
+                        formRenderProps={formProps}
                       />
-                    );
+                    )
                   }}
                 />
 
                 <FormField
-                  name={`grouping`}
                   control={control}
+                  name='grouping'
                   render={(formProps) => {
                     return (
                       <FormSelect
-                        formKey="device_grouping"
                         fieldConfig={{
                           id: `grouping`,
-                          formType: "select",
+                          formType: 'select',
                           name: `grouping`,
-                          label: "Grouping",
+                          label: 'Grouping',
                         }}
-                        formRenderProps={formProps}
                         form={form}
+                        formKey="device_grouping"
+                        formRenderProps={formProps}
                         selectOptions={{
-                          grouping: grouping,
+                          grouping,
                         }}
                       />
-                    );
+                    )
                   }}
                 />
               </div>
 
               <div className="grid grid-cols-1 lg:grid-cols-2">
                 <FormField
-                  name={`country`}
                   control={control}
+                  name='country'
                   render={(formProps) => {
                     return (
                       <FormSelect
-                        formKey="device_country"
                         fieldConfig={{
                           id: `country`,
-                          formType: "select",
+                          formType: 'select',
                           name: `country`,
                           label: `Country`,
                         }}
+                        form={form}
+                        formKey="device_country"
                         formRenderProps={{
                           ...formProps,
 
                           field: {
                             ...formProps.field,
                             onChange: (value) => {
-                              form.setValue("country", value, {
+                              form.setValue('country', value, {
                                 shouldValidate: true,
                                 shouldDirty: true,
-                              });
-                              form.setValue("city", "", {
+                              })
+                              form.setValue('city', '', {
                                 shouldDirty: true,
-                              });
-                              form.setValue("state", "", {
+                              })
+                              form.setValue('state', '', {
                                 shouldDirty: true,
-                              });
+                              })
                             },
                           },
                         }}
-                        form={form}
                         selectOptions={{
                           country: country_options,
                         }}
                       />
-                    );
+                    )
                   }}
                 />
               </div>
               <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                 <FormField
-                  name={`state`}
                   control={control}
+                  name='state'
                   render={(formProps) => {
                     return (
                       <FormSelect
-                        formKey="device_state"
                         fieldConfig={{
                           id: `state`,
-                          formType: "select",
+                          formType: 'select',
                           name: `state`,
                           label: `State/Province`,
                         }}
-                        formRenderProps={formProps}
                         form={form}
+                        formKey="device_state"
+                        formRenderProps={formProps}
                         selectOptions={{
                           state: state_options,
                         }}
                       />
-                    );
+                    )
                   }}
                 />
 
                 <FormField
-                  name={`city`}
                   control={control}
+                  name='city'
                   render={(formProps) => {
                     return (
                       <FormSelect
-                        formKey="device_city"
                         fieldConfig={{
                           id: `city`,
-                          formType: "select",
+                          formType: 'select',
                           name: `city`,
                           label: `City`,
                         }}
-                        formRenderProps={formProps}
                         form={form}
+                        formKey="device_city"
+                        formRenderProps={formProps}
                         selectOptions={{
                           city: city_options,
                         }}
                       />
-                    );
+                    )
                   }}
                 />
               </div>
-            </Fragment>
+            </>
           </FormItem>
-        );
+        )
       }}
     />
-  );
+  )
 }
