7d009cea30a3f8b2950aaa39a8c71e8773a18182
Kit <kitmikhaelbagares@dnamicro.com>
2025-01-31 08:16:01 +0800

    Update: Tab fixed

--- src/components/platform/Tab/Item.tsx
+++ src/components/platform/Tab/Item.tsx
@@ -2,20 +2,24 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React, { Fragment, useMemo } from 'react'
+import React, { Fragment, useEffect, useMemo } from 'react'
 
 import { cn, formatAndCapitalize } from '~/lib/utils'
 
 import CloseTab from './CloseKebab'
+import { api } from '~/trpc/react'
+import { type IActions } from './TabItems'
 
 type ItemProps = {
   tab: any
+  actions: IActions
 }
 
-const Item = ({ tab }: ItemProps) => {
+const Item = ({ tab, actions }: ItemProps) => {
   const padding = tab.name === 'dashboard' ? 'pr-2' : 'pr-0'
-  const checkIfUserRole = (entity: string) => entity === 'user_role' ? true : false
-
+  const checkIfUserRole = (entity: string) =>
+    entity === 'user_role' ? true : false
+  const updateTabs = api.tab.updateMainTabs.useMutation()
   const pathname = usePathname()
   // eslint-disable-next-line no-unsafe-optional-chaining
   const [, , entity] = pathname?.split('/')
@@ -25,13 +29,20 @@ const Item = ({ tab }: ItemProps) => {
     return entityName === entity
   }, [entity])
 
+  useEffect(() => {
+    updateTabs.mutateAsync({
+      tab_name: entity!,
+      is_active: isActive,
+    })
+  }, [isActive])
+
   return (
     <Fragment key={checkIfUserRole(tab.name) ? 'role' : tab.name}>
       <div className="group relative flex items-center">
         <Link
           data-test-id={
-            'mntab-'
-            + (checkIfUserRole(tab.name) ? 'role' : tab.name)
+            'mntab-' +
+            (checkIfUserRole(tab.name) ? 'role' : tab.name)
               .split(' ')
               .join('-')
               .toLowerCase()
@@ -41,12 +52,16 @@ const Item = ({ tab }: ItemProps) => {
           className={cn(
             isActive
               ? 'rounded-t-lg border-b-0 border-l border-r border-t-2 border-t-primary text-primary'
-              : 'text-gray-500', 'max-h-[32px] whitespace-nowrap px-[8px] py-1 text-sm font-medium', 'flex items-center space-x-2 pl-[8px]', 'relative hover:border-t-primary hover:text-primary', padding,
+              : 'text-gray-500',
+            'max-h-[32px] whitespace-nowrap px-[8px] py-1 text-sm font-medium',
+            'flex items-center space-x-2 pl-[8px]',
+            'relative hover:border-t-primary hover:text-primary',
+            padding
           )}
         >
           {formatAndCapitalize(checkIfUserRole(tab.name) ? 'role' : tab.name)}
           {}
-          <CloseTab {...tab} />
+          <CloseTab actions={actions} {...tab} />
         </Link>
 
         {isActive && (
@@ -55,6 +70,6 @@ const Item = ({ tab }: ItemProps) => {
       </div>
     </Fragment>
   )
-};
+}
 
 export default Item
