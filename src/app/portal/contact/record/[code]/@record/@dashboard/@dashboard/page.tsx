ae0fa62d94f756aded46108705f7b2ec1c791fdf
Kit <kitmikhaelbagares@dnamicro.com>
2025-01-31 12:53:04 +0800

    Update dashboard

---
+++ src/app/portal/contact/record/[code]/@record/@dashboard/@dashboard/page.tsx
@@ -0,0 +1,8 @@
+import NewComingSoon from "~/app/coming-soon";
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+const RecordTabContainer = async () => {
+  return <NewComingSoon type="inner-component"/>;
+};
+
+export default RecordTabContainer;
