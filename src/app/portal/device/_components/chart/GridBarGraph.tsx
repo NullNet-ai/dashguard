f61462aef07d744dede72f55bd0651318be385c7
Malyn Gamboa <malyngamboa@dnamicro.com>
2025-01-31 08:37:30 +0800

    modified grid graph

--- src/app/portal/device/_components/chart/GridBarGraph.tsx
+++ src/app/portal/device/_components/chart/GridBarGraph.tsx
@@ -1,63 +1,64 @@
 'use client'
-
 import React from 'react'
-import { Bar, BarChart } from 'recharts'
+import { Bar, BarChart, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { type BarProps } from 'recharts/types/cartesian/Bar'
+import { type ActiveShape } from 'recharts/types/util/types'
 
 import { type ChartConfig, ChartContainer } from '~/components/ui/chart'
 
-const chartData = [
-  { hour: 1, desktop: 100 },
-  { hour: 2, desktop: 100 },
-  { hour: 3, desktop: 100 },
-  { hour: 4, desktop: 100 },
-  { hour: 5, desktop: 100 },
-  { hour: 6, desktop: 100 },
-  { hour: 7, desktop: 100 },
-  { hour: 8, desktop: 100 },
-  { hour: 9, desktop: 100 },
-  { hour: 10, desktop: 100 },
-  { hour: 11, desktop: 100 },
-  { hour: 12, desktop: 100 },
-  { hour: 13, desktop: 100 },
-  { hour: 14, desktop: 0 },
-  { hour: 15, desktop: 0 },
-  { hour: 16, desktop: 100 },
-  { hour: 17, desktop: 100 },
-  { hour: 18, desktop: 100 },
-  { hour: 19, desktop: 100 },
-  { hour: 20, desktop: 100 },
-  { hour: 21, desktop: 100 },
-  { hour: 22, desktop: 100 },
-  { hour: 23, desktop: 100 },
-  { hour: 24, desktop: 100 },
+import { chartData, getBarPath } from './utils/getBarPath'
 
-]
+interface ShapeProps {
+  x: number
+  y: number
+  width: number
+  height: number
+  value: number
+  payload: { hour: string }
+}
 
 const chartConfig = {
-  desktop: {
-    label: 'Desktop',
-    color: '#008000',
-  },
-  mobile: {
-    label: 'Mobile',
+  heartbeats: {
+    label: 'Heartbeats',
     color: '#008000',
   },
 } satisfies ChartConfig
 
+const CustomBarShape = (props: ShapeProps) => {
+  const { x, y, width, height, value, payload } = props
+  const d = getBarPath(x, y, width, height, value, payload.hour)
+  return <path d={d} fill={chartConfig.heartbeats.color} />
+}
+
 export function GridBarGraph() {
   return (
     <ChartContainer
-      className='h-9 w-40 border border-gray-300'
+      className="h-9 w-40 border border-gray-300"
       config={chartConfig}
-      style={{ padding: '2px', margin: 0 }}
+      style={{ margin: '2px', padding: '0', borderRadius: '8px' }}
     >
-      <BarChart
-        accessibilityLayer={true}
-        data={chartData}
-        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-      >
-        <Bar dataKey='desktop' fill='var(--color-desktop)' />
-      </BarChart>
+      <ResponsiveContainer className="h-full w-full">
+        <BarChart
+          barCategoryGap={-4}
+          barGap={-4}
+          data={chartData}
+          margin={{ bottom: 0, left: 0, right: 0, top: 0 }}
+        >
+          <XAxis dataKey="hour" hide={true} />
+          <YAxis hide={true} />
+          <Tooltip />
+          <Bar
+            dataKey="heartbeats"
+            fill={chartConfig.heartbeats.color}
+            maxBarSize={6}
+            minPointSize={0}
+            shape={
+              CustomBarShape as unknown as ActiveShape<BarProps, SVGPathElement>
+            }
+            strokeWidth={0}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </ChartContainer>
   )
 }
