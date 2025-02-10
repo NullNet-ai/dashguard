a0d8f5117059ed01e1b129968d6e0fb8227bab2c
rob caldosa <robcaldosa@dnamicro.com>
2025-02-11 07:26:13 +0800

    Improve Not Found page styling and layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -21,26 +21,26 @@ export default function NotFound() {
             <div>
               <span className="text-primary text-xl font-semibold">404</span>
             </div>
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl ">
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-[54px] ">
               Page not found
             </h1>
-            <p className="mt-2 text-xl leading-7 text-gray-400 mb-4">
+            <p className="mt-4 text-xl leading-7 text-slate-400 mb-4 font-light">
               Sorry, we couldn’t find the page you’re looking for.
             </p>
-            <Button variant={'outline'} className="border border-primary/50 text-primary" size={'xs'}>Try Again</Button>
+            <Button variant={'outline'} className="border border-primary/80 text-primary" size={'xs'}>Try Again</Button>
           </div>
         </main>
-        <div className=" lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
+        <div className="lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4  lg:flex lg:items-center lg:justify-center">
           <Image
             alt=""
-            width={"1080"}
+            width={"900"}
             height={"720"}
             src="/not-found.png"
-            className="absolute lg:inset-0 md:bottom-[-260px] inset-auto h-full w-full object-cover  bottom-[-236px]"
+            className="lg:transform lg:-translate-y-4 lg:translate-x-[100px]"
           />
         </div>
         <footer>
-            <div className="bg-gray-200 fixed bottom-0 py-4 text-xs w-full text-center text-gray-400">
+            <div className="bg-slate-50 fixed bottom-0 py-4 text-xs w-full text-center text-slate-400">
                 <span>All Right Reserved {new Date().getFullYear()} DNA Micro<sup>TM</sup></span>
             </div>
         </footer>
