2623eb7e36e0b2dc7bcb0cbec4dc10bce19616dc
Malyn Gamboa <malyngamboa@dnamicro.com>
2025-02-11 14:05:01 +0800

    update

--- src/app/portal/device/record/_actions/statusOptions.tsx
+++ src/app/portal/device/record/_actions/statusOptions.tsx
@@ -2,24 +2,24 @@
 import { IMenuOptionConfig } from "~/components/platform/Record/types";
 import { handleChangeStatus } from ".";
 const statusOptions = [
-  {
-    label: "Identifier Option One",
-    onClick: handleChangeStatus.bind(null, "Passed"),
-  },
-  {
-    label: "Identifier Option Two",
-    onClick: handleChangeStatus.bind(null, "Test"),
-    children: [
-      {
-        label: "Identifier Option Three",
-        onClick: handleChangeStatus.bind(null, "Test"),
-      },
-      {
-        label: "Identifier Option Four",
-        onClick: handleChangeStatus.bind(null, "Test"),
-      },
-    ],
-  },
+  // {
+  //   label: "Identifier Option One",
+  //   onClick: handleChangeStatus.bind(null, "Passed"),
+  // },
+  // {
+  //   label: "Identifier Option Two",
+  //   onClick: handleChangeStatus.bind(null, "Test"),
+  //   children: [
+  //     {
+  //       label: "Identifier Option Three",
+  //       onClick: handleChangeStatus.bind(null, "Test"),
+  //     },
+  //     {
+  //       label: "Identifier Option Four",
+  //       onClick: handleChangeStatus.bind(null, "Test"),
+  //     },
+  //   ],
+  // },
 ] as IMenuOptionConfig[];
 
 export default statusOptions;
