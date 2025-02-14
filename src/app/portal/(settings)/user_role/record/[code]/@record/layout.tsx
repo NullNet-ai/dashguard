389fd5b72e49a6fd2fd9896ebf93d109e7cdbc6a
Shem Alexis Chavez <shem@dnamicro.com>
2025-02-14 12:13:22 +0800

    Fix

--- src/app/portal/(settings)/user_role/record/[code]/@record/layout.tsx
+++ src/app/portal/(settings)/user_role/record/[code]/@record/layout.tsx
@@ -2,16 +2,11 @@
 import React from "react";
 import { useSearchParams } from "next/navigation";
 
-type LayoutProps = {
-  dashboard: React.ReactNode
-  user_role: React.ReactNode
-  category_details: React.ReactNode
-}
-
-export default function Layout(props: LayoutProps) {
+export default function Layout(props: { current_tab?: React.ReactNode, dashboard?: React.ReactNode }) {
   const searchParams = useSearchParams()
-  const currentTab = searchParams.get('current_tab') ?? 'dashboard'
-  const slot = props[currentTab as keyof LayoutProps]
+  const slotName = searchParams.get('current_tab') ?? 'dashboard'
+
+  const slot = props[slotName as keyof typeof props]
 
   if (!slot) {
     return <div>Coming Soon</div>
