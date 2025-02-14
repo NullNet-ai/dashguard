17c33e8f880f9367de489c800f56338d449d32c4
Shem Alexis Chavez <shem@dnamicro.com>
2025-02-14 12:00:36 +0800

    Fix

--- src/app/portal/(settings)/user_role/record/[code]/@record/layout.tsx
+++ src/app/portal/(settings)/user_role/record/[code]/@record/layout.tsx
@@ -2,20 +2,19 @@
 import React from "react";
 import { useSearchParams } from "next/navigation";
 
-import { type ILayoutProps } from "./types";
+export default function Layout(props: {
+  dashboard: React.ReactNode
+  user_role: React.ReactNode
+  category_details: React.ReactNode
+  [key: string]: React.ReactNode
+}) {
+  const searchParams = useSearchParams()
+  const slot = props[searchParams.get('current_tab') ?? 'dashboard']
 
-const Layout = (props: ILayoutProps) => {
-
-    const searchParams = useSearchParams()
-
-    const slot = props[searchParams.get('current_tab') ?? 'dashboard']
-
-    if (!slot) {
-        return <div>Coming Soon</div>
-    }
-    return <div>
-        {slot}
-    </div> 
+  if (!slot) {
+    return <div>Coming Soon</div>
+  }
+  return <div>
+    {slot}
+  </div> 
 }
-
-export default Layout;
