742d41f5f85fa827e01fbaec7fd2bf352373e722
Juphet Vitualla <jvitualla@dnamicro.com>
2025-02-15 08:54:20 +0800

    fixed mobile issue in mobile record

--- src/components/platform/Record/Summary/Menu/DefaultSummaryMenuOptions.tsx
+++ src/components/platform/Record/Summary/Menu/DefaultSummaryMenuOptions.tsx
@@ -10,6 +10,7 @@ import {
 } from "~/components/ui/dropdown-menu";
 import { getDefaultMenuOptionConfig } from "../../constants";
 import RecursiveMenuItem from "./RecursiveMenuItem";
+import useScreenType from '~/hooks/use-screen-type';
 
 export interface IMemoizedRecordData {
   entityName?: string;
@@ -34,6 +35,8 @@ export default function DefaultSummaryMenuOptions({
   menuOptionConfig,
 }: IDefaultSummaryMenuOptionsProps) {
   const { recordId, entityName } = memoizedRecordData;
+  const screenType = useScreenType()
+  const isMobile = screenType === "md" || screenType === "sm" || screenType === "xs";
 
   const memoizedMenuOptionConfig = useMemo(() => {
     return [
@@ -43,18 +46,20 @@ export default function DefaultSummaryMenuOptions({
   }, [menuOptionConfig]);
 
   return (
-    <DropdownMenu>
+    <DropdownMenu
+    >
       <DropdownMenuTrigger asChild>
-        <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm cursor-pointer">
+        <div className="flex items-center gap-2 px-1 z-50 py-1.5 text-left text-sm cursor-pointer">
           <EllipsisVertical className={`h-4 w-4`} aria-hidden="true" />
         </div>
       </DropdownMenuTrigger>
-      <DropdownMenuContent side='bottom' align="center">
+      <DropdownMenuContent side='bottom' align={!isMobile ? 'center' : 'end'} className='z-[1000]'>
         {title && <DropdownMenuLabel>{title}</DropdownMenuLabel>}
         <RecursiveMenuItem
           recordId={recordId}
           entityName={entityName!}
           menuOptionConfig={memoizedMenuOptionConfig}
+          isMobile={isMobile}
         />
       </DropdownMenuContent>
     </DropdownMenu>
