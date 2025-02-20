01c5c1905d1f7efffa8e24d8e98866c7b81b28af
Malyn Gamboa <malyngamboa@dnamicro.com>
2025-02-20 15:18:33 +0800

    update layout

--- src/app/portal/dashboard/layout.tsx
+++ src/app/portal/dashboard/layout.tsx
@@ -1,9 +1,14 @@
 export default function Layout(props: {children: React.ReactNode, params: any}) {
 
     const { params, children, ...rest } = props
+
+    const {pie_chart, bar_chart} = rest
+    const RenderComponents = [pie_chart, bar_chart]
+
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 pt-0  lg:-mt-4 md:mt-2">
-            {...Object.values(rest)}
+            {/* {...Object.values(rest)} */}
+            {RenderComponents}
         </section>
     );
 }
