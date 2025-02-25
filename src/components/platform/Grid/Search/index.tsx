d350ee0e13500ccd42d294f675df9ac8cf66b129
Juphet vitualla <vituallajuphet1114@gmail.com>
2025-02-25 14:07:08 +0800

    fixed search issue responsve

--- src/components/platform/Grid/Search/index.tsx
+++ src/components/platform/Grid/Search/index.tsx
@@ -18,7 +18,7 @@ export default function Main({ parentType = 'grid', creatable = true, switchable
     <GridSearchProvider>
       {parentType === 'grid'
         ? (
-            <div className="ml-0 mt-0 flex w-full max-w-[100%] flex-col justify-end gap-x-2 sm:mt-0 lg:ml-2 lg:mt-0 lg:w-[40%] lg:max-w-[40%]">
+            <div className="ml-0 mt-0 flex w-full max-w-[100%] flex-col justify-end gap-x-2 sm:mt-0 lg:ml-2 lg:mt-0 lg:w-[55%] lg:max-w-[55%]">
               <div className={cn(`relative flex flex-1 flex-row `, `${switchable ? 'gap-x-2' : ''}`)}>
                 <div className="my-2 h-[40px] w-full md:my-0">
                   <Search gridType={gridType}/>
