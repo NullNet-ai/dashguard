3e5ab28365f6fdf3e44b75c5dade7de170456d71
Irish <irufo@dnamicro.com>
2025-02-25 09:21:19 +0800

    modify openreplay provider

--- src/context/OpenReplay/Provider.tsx
+++ src/context/OpenReplay/Provider.tsx
@@ -6,19 +6,26 @@ import Tracker from '@openreplay/tracker';
 import { OpenReplayContext } from './context';
 import { type OpenReplayProviderProps } from './types';
 
-const tracker = new Tracker({
-  projectKey: process.env.NEXT_PUBLIC_OPEN_REPLAY_PROJECT_KEY ?? '',
-  ingestPoint: process.env.NEXT_PUBLIC_OPEN_REPLAY_INGEST_POINT,
-});
+
 
 export const OpenReplayProvider = ({ children }: OpenReplayProviderProps) => {
   
   useEffect(() => {
-    tracker.start();
+    try {
+      const tracker = new Tracker({
+        projectKey: process.env.NEXT_PUBLIC_OPEN_REPLAY_PROJECT_KEY ?? '',
+        ingestPoint: process.env.NEXT_PUBLIC_OPEN_REPLAY_INGEST_POINT,
+        __DISABLE_SECURE_MODE: true,
+      });
+      tracker.start();
+      console.info('@@@ OpenReplay started');
+    } catch (e) {
+      console.error('@@@ error starting OpenReplay', e);
+    }
   }, []);
 
   return (
-    <OpenReplayContext.Provider value={{ tracker }}>
+    <OpenReplayContext.Provider value={{ tracker: null }}>
       {children}
     </OpenReplayContext.Provider>
   );
