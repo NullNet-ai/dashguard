657bd2b94d871d2960bb6266f964a9b171f5187d
Juphet Vitualla <jvitualla@dnamicro.com>
2025-02-26 12:25:54 +0800

    fixed grid search issue

--- src/components/platform/Grid/Header/index.tsx
+++ src/components/platform/Grid/Header/index.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
     <>
       <div className="flex flex-col-reverse gap-y-4 py-2 pb-0 lg:flex-row lg:justify-between">
-        <div className="flex flex-col justify-between sm:flex-auto lg:flex-row md:max-w-[53%]">
+        <div className="flex flex-col justify-between sm:flex-auto lg:flex-row md:max-w-[49%]">
           <div className="flex-1 w-full flex flex-col"> 
             <div className="flex h-[36px] justify-between">
               <GridTabs />
