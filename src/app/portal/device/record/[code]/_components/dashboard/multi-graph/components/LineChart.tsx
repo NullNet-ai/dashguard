3b5c4e7beaf6a9a47b3aa295a8cc718335fba16b
Irish <irufo@dnamicro.com>
2025-03-04 05:34:27 +0800

    modify default val

--- src/app/portal/device/record/[code]/_components/dashboard/multi-graph/components/LineChart.tsx
+++ src/app/portal/device/record/[code]/_components/dashboard/multi-graph/components/LineChart.tsx
@@ -27,6 +27,7 @@ export const modifyAxis = (chartData:any) => {
   return { yAxisMax, yAxisMin }
 }
 export const formatNumber = (num: number) => {
+  if(num === 0) return ''
   if (num >= 1_000_000) return `${(num / 1_000_000).toFixed(1)}M`
   if (num >= 1_000) return `${(num / 1_000).toFixed(1)}K`
   return num.toString()
