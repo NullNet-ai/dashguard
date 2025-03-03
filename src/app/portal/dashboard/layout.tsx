0fb1b24d57b484762d00715c8f2cbc913fd6fbb0
Malyn Gamboa <malyngamboa@dnamicro.com>
2025-03-04 07:28:20 +0800

    fix build issue

--- src/app/portal/dashboard/layout.tsx
+++ src/app/portal/dashboard/layout.tsx
@@ -1,9 +1,20 @@
-export default function Layout(props: {children: React.ReactNode, params: any}) {
+import React from 'react';
 
-    const { params, children, ...rest } = props
-    return (
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 pt-0  lg:-mt-4 md:mt-2">
-            {...Object.values(rest)}
-        </section>
-    );
+interface LayoutProps {
+  children: React.ReactNode;
+  params: any;
+  contact: string;
+  organization: string;
+}
+
+export default function Layout(props: LayoutProps) {
+  const { params, children, ...rest } = props;
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 pt-0 lg:-mt-4 md:mt-2">
+      {children}
+      {/* <div>Contact: {contact}</div>
+      <div>Organization: {organization}</div> */}
+      {...Object.values(rest)}
+    </section>
+  );
 }
