c23341d051605bfba8250c1c526b6e22978cdd2e
Irish <irufo@dnamicro.com>
2025-03-04 09:29:37 +0800

    added default sort

--- src/app/portal/device/grid/_config/sorting.ts
+++ src/app/portal/device/grid/_config/sorting.ts
@@ -2,7 +2,7 @@ import { SortingState } from "@tanstack/react-table";
 
 export const defaultSorting = [
   {
-    id: "code",
-    desc: true,
+    id: "instance_name",
+    desc: false,
   },
 ] as SortingState;
