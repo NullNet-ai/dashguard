a69fa0b0ec707088c69d574c0f984a581f467559
Irish <irufo@dnamicro.com>
2025-03-08 14:48:53 +0800

    modify bandwidth

--- src/app/portal/device/record/[code]/_components/dashboard/network-traffic-visualization/index.tsx
+++ src/app/portal/device/record/[code]/_components/dashboard/network-traffic-visualization/index.tsx
@@ -9,14 +9,18 @@ import IPNode from './components/IPNode'
 import TrafficNode from './components/TrafficNode'
 import { generateFlowData } from './functions/generateFlowData'
 import { type Edge, type FlowElement } from './types'
+import { getLastTimeStamp } from '~/app/portal/device/utils/timeRange';
 
 export default function NetworkFlow() {
   const [elements, setElements] = useState<{ nodes: FlowElement[], edges: Edge[] }>({ nodes: [], edges: [] })
 
   // const { data: packetsIP, refetch } = api.packet.fetchPacketsIP.useQuery({})
-  // const { data: bandwidth } = api.packet.getBandwidthOfSourceIP.useQuery(
-  //   { packet_data: packetsIP }, { enabled: !!packetsIP }
-  // )
+  const { data: bandwidth } = api.packet.getBandwidthOfSourceIP.useQuery(
+    {
+      device_id: '6cb6c156-e8df-461b-83ec-23aee142a664',
+      time_range: getLastTimeStamp(1, 'minute'),
+    }
+  )
 
   // useEffect(() => {
   //   const fetchData = async () => {
@@ -27,33 +31,30 @@ export default function NetworkFlow() {
   //   fetchData().catch(error => console.error('Error fetching data:', error))
   // }, [packetsIP, refetch])
 
-  // useEffect(() => {
-  //   if (bandwidth) {
-  //     setElements(generateFlowData(bandwidth))
-  //   }
-  // }, [bandwidth])
+
 
   const nodeTypes = useMemo(
     () => ({ ipNode: IPNode, trafficNode: TrafficNode }), []
   )
 
   return (
-    <div className="p-4">
-      <div className="h-[calc(100vh-100px)] rounded-lg border border-gray-200 bg-white shadow-lg">
-        <ReactFlow
-          defaultViewport={{ x: 0, y: 0, zoom: 0.8 }}
-          edges={elements.edges}
-          fitView={true}
-          maxZoom={1.5}
-          minZoom={0.1}
-          nodes={elements.nodes}
-          nodesDraggable={true}
-          nodeTypes={nodeTypes}
-        >
-          <Background color="#f1f5f9" />
-          <Controls />
-        </ReactFlow>
-      </div>
-    </div>
+    <></>
+    // <div className="p-4">
+    //   <div className="h-[calc(100vh-100px)] rounded-lg border border-gray-200 bg-white shadow-lg">
+    //     <ReactFlow
+    //       defaultViewport={{ x: 0, y: 0, zoom: 0.8 }}
+    //       edges={elements.edges}
+    //       fitView={true}
+    //       maxZoom={1.5}
+    //       minZoom={0.1}
+    //       nodes={elements.nodes}
+    //       nodesDraggable={true}
+    //       nodeTypes={nodeTypes}
+    //     >
+    //       <Background color="#f1f5f9" />
+    //       <Controls />
+    //     </ReactFlow>
+    //   </div>
+    // </div>
   )
 }
--- src/server/api/routers/packet.ts
+++ src/server/api/routers/packet.ts
@@ -496,46 +496,46 @@ export const packetRouter = createTRPCRouter({
       return { source_ip, destination_ip, result: res?.data }
     }, { concurrency: 10 })
   }),
-  getBandwidthOfSourceIP: privateProcedure.input(z.object({ packet_data: z.any() })).query(async ({ input, ctx }) => {
-    const { packet_data } = input
-    return await Bluebird.map(packet_data, async (item: { source_ip: string }) => {
-      const { source_ip } = item
+  // getBandwidthOfSourceIP: privateProcedure.input(z.object({ packet_data: z.any() })).query(async ({ input, ctx }) => {
+  //   const { packet_data } = input
+  //   return await Bluebird.map(packet_data, async (item: { source_ip: string }) => {
+  //     const { source_ip } = item
 
-      const res = await ctx.dnaClient.aggregate({
-      // @ts-expect-error - entity is not defined in the type
-        query: {
-          entity: 'packets',
-          aggregations: [
-            {
-              aggregation: 'SUM',
-              aggregate_on: 'total_length',
-              bucket_name: 'bandwidth',
-            },
-          ],
-          advance_filters: [
-            {
-              type: 'criteria',
-              field: 'source_ip',
-              entity: 'packets',
-              operator: EOperator.EQUAL,
-              values: [
-                source_ip,
-              ],
-            },
-          ],
-          joins: [],
-          limit: 20,
-          order: {
-            order_by: 'bucket',
-            order_direction: EOrderDirection.DESC,
-          },
-        },
-        token: ctx.token.value,
-      }).execute()
+  //     const res = await ctx.dnaClient.aggregate({
+  //     // @ts-expect-error - entity is not defined in the type
+  //       query: {
+  //         entity: 'packets',
+  //         aggregations: [
+  //           {
+  //             aggregation: 'SUM',
+  //             aggregate_on: 'total_length',
+  //             bucket_name: 'bandwidth',
+  //           },
+  //         ],
+  //         advance_filters: [
+  //           {
+  //             type: 'criteria',
+  //             field: 'source_ip',
+  //             entity: 'packets',
+  //             operator: EOperator.EQUAL,
+  //             values: [
+  //               source_ip,
+  //             ],
+  //           },
+  //         ],
+  //         joins: [],
+  //         limit: 20,
+  //         order: {
+  //           order_by: 'bucket',
+  //           order_direction: EOrderDirection.DESC,
+  //         },
+  //       },
+  //       token: ctx.token.value,
+  //     }).execute()
 
-      return { source_ip, result: res?.data }
-    }, { concurrency: 10 })
-  }),
+  //     return { source_ip, result: res?.data }
+  //   }, { concurrency: 10 })
+  // }),
   filterPackets: privateProcedure.input(z.record(z.unknown())).query(async ({ input, ctx }) => {
     const {
       limit = 50,
@@ -604,4 +604,173 @@ export const packetRouter = createTRPCRouter({
       totalPages,
     }
   }),
+  getBandwidthOfSourceIP: privateProcedure.input(z.object({device_id: z.string(), time_range: z.array(z.string())})).query(async ({ input, ctx }) => {
+
+    
+   const { device_id, time_range } = input
+   let source_ips: string[] = []
+   
+   const filterPackets = async(starts_at: number) => {
+     const limit = 1000
+     
+     console.log('%c Line:612 🥖 source_ips', 'color:#ffdd4d', source_ips);
+
+      const { account } = ctx.session
+      const { contact } = account
+
+      const { filter = [], search =[] }: any = await Promise.all(['filter', 'search'].map(async type => await ctx.redisClient.getCachedData(`timeline_${type}_${contact.id}`)))
+
+      // const filter_id = '01JNQTACVP5MR3TBZVZGMY6QCH'
+      // const findFilter = filter?.find((item: any) => item?.id === filter_id)
+      // const _filter = findFilter?.default_filter || []
+      // const custom_adv = [
+      //   ..._filter,
+      //   {
+      //     type: 'operator',
+      //     operator: EOperator.AND,
+      //   },
+      //   ...search,
+      //   {
+      //     type: 'operator',
+      //     operator: EOperator.AND,
+      //   },
+      // ]
+
+   
+      const packets =  await ctx.dnaClient
+      .findAll({
+        entity: 'packets',
+        token: ctx.token.value,
+        query: {
+          track_total_records: true,
+          pluck: ['source_ip', 'id', 'device_id', 'timestamp'],
+          advance_filters: [
+            // ...custom_adv,
+            {
+              type: 'criteria',
+              field: 'timestamp',
+              entity: 'packets',
+              operator: EOperator.IS_BETWEEN,
+              values: time_range,
+            },
+            {
+              type: 'operator',
+              operator: EOperator.AND,
+            },
+            {
+              type: 'criteria',
+              field: 'device_id',
+              entity: 'packets',
+              operator: EOperator.EQUAL,
+              values: [device_id],
+            },
+            ...( source_ips?.length ? [{
+              type: 'operator',
+              operator: EOperator.AND,
+            },
+            {
+              type: 'criteria',
+              field: 'source_ip',
+              entity: 'packets',
+              operator: EOperator.NOT_CONTAINS,
+              values: source_ips,
+            }] : [])
+          ],
+          order: {
+            starts_at,
+            limit,
+            by_field: 'code',
+            by_direction: EOrderDirection.DESC,
+          },
+        },
+  
+      })
+      .execute()
+
+      const _packets = packets?.data || []
+      const _packets_length = _packets.length
+
+      const sourceIPs = new Set();
+      for (let i = 0; i < _packets_length; i++) {
+        sourceIPs.add(_packets[i].source_ip);
+    }
+    
+
+    source_ips = [...new Set([...source_ips, ...sourceIPs])] as string[]      
+    console.log('%c Line:697 🍋 source_ips', 'color:#4fff4B', source_ips);
+
+      if(_packets_length == limit){
+        const new_start = starts_at + limit
+        await filterPackets(new_start)
+      }
+      
+
+
+      
+    }
+  
+
+
+   await filterPackets(0)
+
+    console.log('%c Line:715 🍧', 'color:#ffdd4d', JSON.stringify(source_ips));
+
+
+
+    const ab =  await Bluebird.map(source_ips, async (source_ip: string) => {
+      console.log('%c Line:721 🍔 source_ip', 'color:#fca650', source_ip); 
+
+      const res = await ctx.dnaClient.aggregate({
+        query: {
+          entity: 'packets',
+          aggregations: [
+            {
+              aggregation: 'SUM',
+              aggregate_on: 'total_length',
+              bucket_name: 'bandwidth',
+            },
+          ],
+          advance_filters: [
+            {
+              type: 'criteria',
+              field: 'timestamp',
+              entity: 'packets',
+              operator: EOperator.IS_BETWEEN,
+              values: time_range,
+            },
+            {
+              type: 'operator',
+              operator: EOperator.AND,
+            },
+            {
+              type: 'criteria' as const,
+              field: 'source_ip',
+              entity: 'packets',
+              operator: EOperator.EQUAL,
+              values: [
+                source_ip,
+              ],
+            },
+          ],
+          joins: [],
+          bucket_size: '5s',
+          order: {
+            order_by: 'bucket',
+            order_direction: EOrderDirection.DESC,
+          },
+          // timezone,
+        },
+        token: ctx.token.value,
+  
+      }).execute()
+
+      console.log('%c Line:811 🍅', 'color:#b03734', JSON.stringify({ source_ip, result: res?.data },null,2));
+
+      return { source_ip, result: res?.data }
+    }, { concurrency: 10 })
+
+    console.log('%c Line:804 🍓 ab', 'color:#7f2b82', JSON.stringify(ab,null,2 ));
+    return ab
+  }),
+
 })
