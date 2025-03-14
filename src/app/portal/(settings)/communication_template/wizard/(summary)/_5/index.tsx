eb948fd9a4ea84aa3096b027f589898eaedd1d50
Mary Grace Tiburillo <mgtiburillo@dnamicro.com>
2025-03-15 05:58:51 +0800

    Update: communication template changes

--- src/app/portal/(settings)/communication_template/wizard/(summary)/_5/index.tsx
+++ src/app/portal/(settings)/communication_template/wizard/(summary)/_5/index.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+const Summary = () => {
+  return null
+}
+
 const SummaryConfig = {
   label: 'Step 5',
   required: true,
   components: [
     {
       label: 'Confirmation',
+      component: <Summary />,
     },
   ],
 };
