31b5c0a30fb41db4f582e24f7cbdbeacba06ce60
rob caldosa <robcaldosa@dnamicro.com>
2025-03-18 09:48:22 +0800

    feat: add favorite menu configuration and update menu arrangement

--- src/server/menu/index.ts
+++ src/server/menu/index.ts
@@ -1,16 +1,12 @@
-  import activity_log from "./activity_log";
   import contact from "./contact";
   import dashboard from "./dashboard";
-  import favorite from "./favorite";
   import organization from "./organization";
   import organization_account from "./organization_account";
   import settings from "./settings";
 
 const menu = [
-  activity_log,
   contact,
   dashboard,
-  favorite,
   organization,
   organization_account,
   settings,
