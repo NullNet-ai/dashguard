cc6fc4d49bc4dbc50956ddc4e9b6993f5ba06810
Juphet Vitualla <jvitualla@dnamicro.com>
2025-04-12 08:03:07 +0800

    feat(loading): add consistent loading spinner across portal pages
    Introduce a standardized loading spinner component using the  UI component across various portal pages. This ensures a consistent user experience during data fetching operations. The spinner is centered on the page and includes a label indicating the data fetching process.

--- src/app/portal/organization/record/[code]/@record/@organization/@organizations/loading.tsx
+++ src/app/portal/organization/record/[code]/@record/@organization/@organizations/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from '~/components/ui/loader'
 
 export default function Loading () {
   return (
-   <div className='w-full flex items-center justify-center h-full min-h-[calc(100dvh-100px)]'>
+   <div className='w-full flex items-center justify-center h-full min-h-[calc(100dvh-150px)]'>
          <Loader
             className="bg-primary text-primary"
             label="Fetching data..."
