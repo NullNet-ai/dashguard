e3effd039f0f9aac61e0907863d228b6ebec6e7c
Malyn Gamboa <malyngamboa@dnamicro.com>
2025-05-14 06:22:56 +0800

    Update socket url

--- src/app/portal/device/record/[code]/_components/dashboard/custom-hooks/useSocketConnection.ts
+++ src/app/portal/device/record/[code]/_components/dashboard/custom-hooks/useSocketConnection.ts
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://datastore.nullnetqa.net:5001';
 // const SOCKET_URL = 'http://localhost:5001';
 
 export function useSocketConnection({channel_name, token}: {channel_name?: string, token: string | null}) {
@@ -13,7 +12,7 @@ export function useSocketConnection({channel_name, token}: {channel_name?: strin
   useEffect(() => {
     if (!token) return;
 
-    const socket: any = io(SOCKET_URL, {
+    const socket: any = io(`${process.env.NEXT_PUBLIC_SOCKET_URL}:5001`, {
       transports: ['websocket'],
       auth: { token: token },
       autoConnect: true,
@@ -31,9 +30,8 @@ export function useSocketConnection({channel_name, token}: {channel_name?: strin
       setIsConnected(false);
     });
 
-    console.debug('%c Line:35 🥒 socket', 'color:#4fff4B', socket);
     socket.on('connection_multi_graph-07da2369-432b-48f4-8714-372f9412ff57', (data: any) => {
-      console.debug('%c Line:35 🍌 data', 'color:#3f7cff', data);
+
     })
     
     socket.on('connect_error', (err: any) => {
