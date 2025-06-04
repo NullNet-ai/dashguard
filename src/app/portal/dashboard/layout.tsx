f72d79bc0e012df7a37bf120c5d64e94c780fb47
Malyn Gamboa <malyngamboa@dnamicro.com>
2025-06-05 07:02:25 +0800

    update menu arrangement

--- src/app/portal/dashboard/layout.tsx
+++ src/app/portal/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import { cn } from '~/lib/utils';
 
 export default function Layout(props: { children: React.ReactNode, params: any }) {
 
-    const {  ...rest } = props
+    const { params, children, ...rest } = props
     return (
         <section className={cn("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 pt-0  md:mt-2",
         )}>
