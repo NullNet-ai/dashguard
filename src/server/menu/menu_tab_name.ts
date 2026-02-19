0bda9fd1920ab076cbcefd7bbe1db837eb9919d8
Shem Alexis Chavez <shem@dnamicro.com>
2026-02-19 21:12:59 +0800

    docs(menu): add comment for project-level menu tab
    Add clarifying comment to indicate that the new device_remote_access_session entry is a project-level menu tab.

--- src/server/menu/menu_tab_name.ts
+++ src/server/menu/menu_tab_name.ts
@@ -1,4 +1,6 @@
 export const menu_tab_name: Record<string, string> = {
   account_organization: 'Accounts',
   user_role: 'Roles',
+  // Project Level
+  device_remote_access_session: 'Remote Access',
 }
