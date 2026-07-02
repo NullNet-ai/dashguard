b277e017840f4330cdaf054eabfaef012ec61c40
Shem Alexis Chavez <shem@dnamicro.com>
2026-07-02 22:53:24 +0800

    feat(menu): update getMenuConfig to accept role parameter and filter menu items accordingly

--- src/components/application-layout/SideBarMenu/config.tsx
+++ src/components/application-layout/SideBarMenu/config.tsx
@@ -1,6 +1,8 @@
 import { type ISidebarMenu } from "~/components/platform/SideBar/type";
 import { api } from "~/trpc/server";
 
-export const MainMenuConfig = async (): Promise<ISidebarMenu[]> => {
-  return await api.menu.getMenuConfig();
+export const MainMenuConfig = async (
+  role?: string | null,
+): Promise<ISidebarMenu[]> => {
+  return await api.menu.getMenuConfig({ role });
 };
