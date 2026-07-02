168a79b5921afdc74343188ae792d3b7f72f7e54
Shem Alexis Chavez <shem@dnamicro.com>
2026-07-02 23:19:39 +0800

    feat: add TypeScript ignore comments for missing types in various components

--- src/app/portal/contact/wizard/(summary)/_5/index.tsx
+++ src/app/portal/contact/wizard/(summary)/_5/index.tsx
@@ -39,6 +39,7 @@ const Summary = ({ form_key }: { form_key: string }) => {
       {Object.entries(fields).map(([key, value]) => (
         <p key={key} className="mb-[8px] no-underline text-[#334155]">
           <span className='text-slate-400'> {value}: </span>
+          {/* @ts-ignore-error - No type yet */}
           &nbsp; {data?.[key as keyof IAccountDetails] || 'None'}
         </p>
       ))}
