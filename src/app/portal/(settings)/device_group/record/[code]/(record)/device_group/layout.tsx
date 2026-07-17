9728fd8779023f3d8d28923f7230b2d86eb664bd
Shem Alexis Chavez <shem@dnamicro.com>
2026-07-17 08:46:40 +0800

    feat: simplify Layout component by removing unused props and consolidating structure

--- src/app/portal/(settings)/device_group/record/[code]/(record)/device_group/layout.tsx
+++ src/app/portal/(settings)/device_group/record/[code]/(record)/device_group/layout.tsx
@@ -1,23 +1,7 @@
-import React, { type ReactNode } from 'react'
+import React, { type ReactNode } from 'react';
 
-type LayoutProps = {
-  children?: ReactNode
-  params?: any
-  device_group?: ReactNode
-  category_details?: ReactNode
-  [key: string]: ReactNode | undefined
-}
+export const dynamic = 'force-dynamic';
 
-export const dynamic = 'force-dynamic'
-
-export default function Layout(props: {
-  device_group: React.ReactNode
-  category_details: React.ReactNode
-}) {
-  return (
-    <div className='space-y-2'>
-      {props.device_group}
-      {props.category_details}
-    </div>
-  )
+export default function Layout(props: { device_group: ReactNode }) {
+  return <div className="space-y-2">{props.device_group}</div>;
 }
